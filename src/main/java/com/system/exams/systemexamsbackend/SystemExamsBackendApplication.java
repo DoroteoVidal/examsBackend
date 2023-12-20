faa020ed9fb04b0250caf99004987b2eb78266c6
Doroteo Vidal <doroteo17vidal@hotmail.com>
2023-12-20 20:19:32 -0300

    first commit

---
+++ src/main/java/com/system/exams/systemexamsbackend/SystemExamsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.system.exams.systemexamsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemExamsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemExamsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/exams/systemexamsbackend/SystemExamsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.exams.systemexamsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemExamsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
