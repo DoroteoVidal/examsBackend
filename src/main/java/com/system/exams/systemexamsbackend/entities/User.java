0a03a476572953c583e4cdda26417448e8d7f812
Doroteo Vidal <doroteo17vidal@hotmail.com>
2023-12-20 21:04:19 -0300

    se agregan entidades user

---
+++ src/main/java/com/system/exams/systemexamsbackend/entities/User.java
@@ -0,0 +1,39 @@
+package com.system.exams.systemexamsbackend.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String name;
+    private String surname;
+    private String email;
+    private String phone;
+    private boolean enabled = true;
+    private String profile;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "user")
+    private Set<UserRole> userRoles = new HashSet<>();
+
+    public User() {}
+
+}
