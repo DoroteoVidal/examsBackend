b9329bee11c233acdebe91747adf26401766cf51
Doroteo Vidal <doroteo17vidal@hotmail.com>
2024-01-09 10:32:10 -0300

    se agrega metodo de filtro

--- src/main/java/com/system/exams/systemexamsbackend/config/AuthAuthenticacionFilter.java
+++ src/main/java/com/system/exams/systemexamsbackend/config/AuthAuthenticacionFilter.java
@@ -1,6 +1,11 @@
 package com.system.exams.systemexamsbackend.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -8,6 +13,7 @@ import com.system.exams.systemexamsbackend.services.impl.UserDetailsServiceImpl;
 
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -21,7 +27,7 @@ public class AuthAuthenticacionFilter extends OncePerRequestFilter {
     private JwtUtils jwtUtils;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException {
         String requestTokenHeader = request.getHeader("Authorization");
         String username = null;
         String jwtToken = null;
@@ -37,6 +43,14 @@ public class AuthAuthenticacionFilter extends OncePerRequestFilter {
             }
         }else {
             System.out.println("Invalid token, does not start with bearer");
+        }
+
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsServiceImpl.loadUserByUsername(username);
+            if(this.jwtUtils.validateToken(jwtToken, userDetails)) {
+                UsernamePasswordAuthenticationToken upat = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                upat.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            }
         }
     }
     
