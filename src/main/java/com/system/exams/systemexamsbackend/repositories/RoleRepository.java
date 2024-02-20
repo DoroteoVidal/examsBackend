3a6c865f158a23cb067219d001274e6b1308fc3c
Doroteo Vidal <doroteo17vidal@hotmail.com>
2024-02-20 09:32:22 -0300

    se agrega documentacion a interfaz usuario

--- src/main/java/com/system/exams/systemexamsbackend/repositories/RoleRepository.java
+++ src/main/java/com/system/exams/systemexamsbackend/repositories/RoleRepository.java
@@ -1,7 +1,9 @@
 package com.system.exams.systemexamsbackend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.system.exams.systemexamsbackend.entities.Role;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {}
--- src/main/java/com/system/exams/systemexamsbackend/repositories/UserRepository.java
+++ src/main/java/com/system/exams/systemexamsbackend/repositories/UserRepository.java
@@ -3,12 +3,29 @@ package com.system.exams.systemexamsbackend.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.system.exams.systemexamsbackend.entities.User;
 
+/**
+ * Interfaz de la clase Usuario para implementar metodos CRUD y metodos propios de la clase
+ * @author Vidal Doroteo
+ */
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    /**
+     * 
+     * @param username nombre de usuario que se utilizara para buscar un Usuario.
+     * @return Usuario encontrado.
+     */
     public User findByUsername(String username);
 
+    /**
+     * 
+     * @param email email de usuario que se utilizara para buscar un Usuario.
+     * @return Optional<User> encontrado.
+     */
     public Optional<User> findUserByEmailIgnoreCase(String email);
 }
